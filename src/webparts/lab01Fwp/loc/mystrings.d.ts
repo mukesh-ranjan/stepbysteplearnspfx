fa2c7c2985596a1eb5df96e9cc5ed9937b68d355
unknown <ranjan.muku@gmail.com>
2022-06-07 08:05:05 +0530

    LAB01

---
+++ src/webparts/lab01Fwp/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ILab01FwpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'Lab01FwpWebPartStrings' {
+  const strings: ILab01FwpWebPartStrings;
+  export = strings;
+}
