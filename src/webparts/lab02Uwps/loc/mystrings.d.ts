1e8ba8ca64688f5f3fe09a5fa60302847dcc5868
unknown <ranjan.muku@gmail.com>
2022-06-07 08:18:43 +0530

    LAB02

---
+++ src/webparts/lab02Uwps/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ILab02UwpsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'Lab02UwpsWebPartStrings' {
+  const strings: ILab02UwpsWebPartStrings;
+  export = strings;
+}
