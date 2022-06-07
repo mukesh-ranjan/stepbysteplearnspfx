a270ecb3969a4b987389862bce71df245a0a00ee
unknown <ranjan.muku@gmail.com>
2022-06-07 08:21:36 +0530

    LAB03

---
+++ src/webparts/lab03La1/Lab03La1WebPart.ts
@@ -0,0 +1,88 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './Lab03La1WebPart.module.scss';
+import * as strings from 'Lab03La1WebPartStrings';
+
+export interface ILab03La1WebPartProps {
+  description: string;
+}
+
+export default class Lab03La1WebPart extends BaseClientSideWebPart<ILab03La1WebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+
+  protected onInit(): Promise<void> {
+    this._environmentMessage = this._getEnvironmentMessage();
+
+    return super.onInit();
+  }
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <section class="${styles.lab03La1} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+      <div class="${styles.welcome}">
+       <p class="${styles.welcome}">Absolute URL ${escape(this.context.pageContext.web.absoluteUrl)}  </p>
+       <p class="${styles.welcome}">Title ${escape(this.context.pageContext.web.title)}  </p>
+       <p class="${styles.welcome}">Server Relative Url ${escape(this.context.pageContext.web.serverRelativeUrl)}  </p>
+       <p class="${styles.welcome}">Display Name ${escape(this.context.pageContext.user.displayName)}  </p>
+      </div>
+    </section>`;
+  }
+
+  private _getEnvironmentMessage(): string {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams
+      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    }
+
+    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+    this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
+    this.domElement.style.setProperty('--link', semanticColors.link);
+    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
+
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/lab03La1/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ILab03La1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'Lab03La1WebPartStrings' {
+  const strings: ILab03La1WebPartStrings;
+  export = strings;
+}
